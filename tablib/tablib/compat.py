b21f6efabef2821c62cea3ffac75e1215be94d76
Manish <itzmanish108@gmail.com>
2018-12-30 18:44:44 +0530

    added custom tablib for xlsx export fix

---
+++ tablib/tablib/compat.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+
+"""
+tablib.compat
+~~~~~~~~~~~~~
+
+Tablib compatiblity module.
+
+"""
+
+import sys
+
+is_py3 = (sys.version_info[0] > 2)
+
+
+
+try:
+    from collections import OrderedDict
+except ImportError:
+    from tablib.packages.ordereddict import OrderedDict
+
+
+if is_py3:
+    from io import BytesIO
+    from itertools import zip_longest as izip_longest
+    from statistics import median
+    from tablib.packages import markup3 as markup
+    import tablib.packages.dbfpy3 as dbfpy
+
+    import csv
+    from io import StringIO
+    # py3 mappings
+
+    ifilter = filter
+    unicode = str
+    bytes = bytes
+    basestring = str
+    xrange = range
+
+else:
+    from cStringIO import StringIO as BytesIO
+    from cStringIO import StringIO
+    from tablib.packages import markup
+    from tablib.packages.statistics import median
+    from itertools import ifilter, izip_longest
+
+    import unicodecsv as csv
+    import tablib.packages.dbfpy as dbfpy
+
+    unicode = unicode
+    xrange = xrange
